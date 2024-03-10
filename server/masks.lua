72f91152db3a53dda6d8125f51efa46de95a50dd
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-10 14:38:16 -0500

    Update masks.lua

--- server/masks.lua
+++ server/masks.lua
@@ -1,8 +1,11 @@
-exports('meta_mask', function(event, item, inventory, slot, data)
-    if event == 'usingItem' then
-        if item.name == 'meta_mask' then
-            local itemSlot = exports.ox_inventory:GetSlot(inventory.id, slot)
-            TriggerClientEvent('dd5m_metamasks:client:maskAction', inventory.id, item, slot, itemSlot.metadata)
-        end
-    end
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- Useable mask item
+QBCore.Functions.CreateUseableItem('mask_example', function(source, item)
+    local src = source
+    -- Assuming maskId and maskType are stored in item metadata. Adjust these keys according to your actual item structure.
+    TriggerClientEvent('meta_masks:applyMask', src, item.info.maskId, item.info.maskType)
 end)
+
+-- Example for creating more useable mask items. Repeat as necessary for each mask type.
+-- QBCore.Functions.CreateUseableItem('another_mask_item', function(source, item) ...
